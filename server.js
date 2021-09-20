bacc94b795b5e5f0f74655caef6b761620896926
neoWolverine <89948799+neoWolverine@users.noreply.github.com>
2021-09-20 18:42:30 -0500

    MULTIPLAYER

---
+++ server.js
@@ -0,0 +1,7 @@
+const PORT = process.env.PORT || 3000;
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+}).listen(PORT);
