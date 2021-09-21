b6c60938f80184fe7634edd6d4fa7f3f1fe8df9c
neoWolverine <89948799+neoWolverine@users.noreply.github.com>
2021-09-20 20:02:07 -0500

    sxsx

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const PORT = process.env.PORT || 3000;
 var http = require("http");
+var tablero = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 http
   .createServer(function (req, res) {
@@ -10,15 +11,25 @@ http
 
 
     if (req.url.lastIndexOf("juega") !== -1) {
-      // res.write("A jugar papus");
       var inicio = req.url.lastIndexOf("juega") + 5;
       var fin = inicio + 49;
       var texto = req.url;
-      texto = texto.substring(inicio,fin);
-      res.write(texto);
+      texto = texto.substring(inicio, fin);
+      var valores = texto.split(",");
+      for (var i = 0; i < 25; i++) {
+        
+        if (valores[i] >= 0 && valores[i] <= 2) {
+          valores[i] = parseInt(valores[i]);
+          
+        }else{
+          valores[i] = 0;
+        }
+      }
+      tablero=valores;
+      res.write(JSON.stringify(tablero));
 
     } else {
-      res.write("Oh nooooo");
+      res.write(JSON.stringify(tablero));
     }
     res.end();
   })
