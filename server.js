ea91b80d4509a5f3342847f25c98f76792ebd285
neoWolverine <89948799+neoWolverine@users.noreply.github.com>
2021-09-20 19:13:42 -0500

    3.0

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const PORT = process.env.PORT || 3000;
-var http = require('http');
+var http = require("http");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(req.url);
-  res.end();
-  
-})
-.listen(PORT);
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+
+
+    if (req.url.lastIndexOf("juega")) {
+      res.write("Prueba prros");
+ 
+    }else{
+      res.write("Que pedooooo");
+    }
+    res.end();
+  })
+  .listen(PORT);
